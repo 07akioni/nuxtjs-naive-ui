8b6415a979340d413d5ce4b1421f01b7fd360991
07akioni <07akioni2@gmail.com>
2024-05-05 16:53:58 +0800

    fix: setup `transpile` to make app can be previewed & build without runtime error

--- src/module.ts
+++ src/module.ts
@@ -13,6 +13,16 @@ export default defineNuxtModule<ModuleOptions>({
   setup(_options, _nuxt) {
     _nuxt.options.nitro.plugins = _nuxt.options.nitro.plugins || []
 
+    // Setup transpile
+    const transpile = process.env.NODE_ENV === 'production' ? ['naive-ui'] : []
+    if (!_nuxt.options.build.transpile) {
+      _nuxt.options.build.transpile = transpile
+    }
+    else {
+      _nuxt.options.build.transpile
+        = _nuxt.options.build.transpile.concat(transpile)
+    }
+
     const resolver = createResolver(import.meta.url)
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
