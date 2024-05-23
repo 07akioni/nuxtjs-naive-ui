cf742846db96d72a6128191f8956bfad4e0e6014
Sandros94 <sandro.circi@digitoolmedia.com>
2024-05-23 14:05:43 +0200

    refactor!: native nuxt auto-imports

--- playground/app.vue
+++ playground/app.vue
@@ -1,9 +1,39 @@
 <template>
   <NConfigProvider inline-theme-disabled>
-    <NButton>Hello World</NButton>
+    <NSpace vertical>
+      <NSwitch v-model:value="loading" />
+      <NCard>
+        <template #header>
+          <NSkeleton
+            v-if="loading"
+            text
+            width="60%"
+          />
+          <template v-else>
+            Lorem Ipsum
+          </template>
+        </template>
+        <NSkeleton
+          v-if="loading"
+          text
+          :repeat="6"
+        />
+        <template v-else>
+          Lorem ipsum dolor sit amet,<br>
+          consectetur adipiscing elit,<br>
+          sed do eiusmod tempor incididunt<br>
+          ut labore et dolore magna aliqua.<br>
+          Ut enim ad minim veniam,<br>
+          quis nostrud exercitation ullamco
+        </template>
+      </NCard>
+    </NSpace>
+    <NNotificationProvider>
+      <NotificationTest />
+    </NNotificationProvider>
   </NConfigProvider>
 </template>
 
 <script setup lang="ts">
-import { NButton, NConfigProvider } from 'naive-ui'
+const loading = ref(true)
 </script>
